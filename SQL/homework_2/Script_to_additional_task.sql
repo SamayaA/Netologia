4cfe22cd59f2b716e4d2ac17e23fd147b6a032a3
SamayaA <asadullaevasamaya@gmail.com>
2021-09-19 19:38:41 +0300

    added head to department and changed name of relation d-e

--- SQL/homework_2/Script_to_additional_task.sql
+++ SQL/homework_2/Script_to_additional_task.sql
@@ -5,10 +5,11 @@ name varchar (80) not null
 
 CREATE TABLE IF NOT EXISTS department (
 id serial not null primary key ,
-title varchar (80) not null
+title varchar (80) not null ,
+head_id integer references employee(id)
 );
 
-CREATE TABLE IF NOT EXISTS department_head (
+CREATE TABLE IF NOT EXISTS department_employee (
 department_id integer references department(id),
 employee_id integer references employee(id)
 );
