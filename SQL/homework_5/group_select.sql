b4f94d30e6f2b177a35167b809da4ab2040f5621
SamayaA <asadullaevasamaya@gmail.com>
2021-10-02 22:14:27 +0300

    Simplified SQL query. Changed select to join.

--- SQL/homework_5/group_select.sql
+++ SQL/homework_5/group_select.sql
@@ -1,49 +1,45 @@
--- count performer by genre
-SELECT  genre.title , COUNT(*) FROM genre , genre_to_performer gtp
-WHERE genre.id = gtp.genreid 
+-- 1. count performer by genre
+SELECT  title , COUNT(gtp.genreid) FROM genre
+JOIN genre_to_performer gtp ON genre.id = gtp.genreid 
 GROUP BY genre.title ;
--- count tracks which belongs to albums released in (2019 - 2020)
-SELECT COUNT(*) FROM track t, album 
-WHERE (album.release_date BETWEEN 2019 and 2020) and 
-t.albumid = album.id ;
--- avg duration of tracks by album
-SELECT album.title , AVG(track.duration) FROM album, track 
-WHERE track.albumid = album.id 
-GROUP BY album.title ;
--- performers didn't release albums in 2020
+-- 2. count tracks which belongs to albums released in (2019 - 2020)
+SELECT COUNT(albumid) FROM album a
+JOIN track t ON t.albumid = a.id
+WHERE a.release_date BETWEEN 2019 and 2020;
+-- 3. avg duration of tracks by album
+SELECT a.title , AVG(t.duration) FROM album a
+LEFT JOIN track t ON t.albumid = a.id 
+GROUP BY a.title ;
+-- 4. performers didn't release albums in 2020
 SELECT performer.name FROM performer 
 WHERE performer.id NOT IN (
-	SELECT performerid FROM album , album_to_performer atp 
-	WHERE album.id = atp.albumid AND album.release_date = 2020);
+	SELECT performerid FROM album a 
+	LEFT JOIN album_to_performer atp ON a.id = atp.albumid 
+	WHERE a.release_date = 2020);
 
--- collections of certain performer
-SELECT DISTINCT collection.name FROM collection , track_to_collection ttc 
-WHERE collection.id = ttc.collectionid AND ttc.trackid IN(
-	SELECT track.id FROM track
-	WHERE track.albumid IN (
-		SELECT albumid FROM album_to_performer atp 
-		WHERE performerid = 2));
--- albums written by performer of different genres
-SELECT album.title FROM album
-WHERE album.id IN (
-	SELECT DISTINCT album_to_performer.albumid FROM album_to_performer , genre_to_performer gtp
-	WHERE gtp.performerid = album_to_performer.performerid
-	GROUP BY album_to_performer.albumid
-	having COUNT(gtp.performerid)>1 
-);
--- tracks that not included in collections
+-- 5. collections of certain performer
+SELECT DISTINCT collection.name FROM collection 
+LEFT JOIN track_to_collection ttc ON collection.id = ttc.collectionid
+LEFT JOIN track t ON t.id = ttc.trackid
+FULL JOIN album_to_performer atp ON t.albumid = atp.albumid
+WHERE atp.performerid = 2;
+-- 6. albums written by performer of different genres
+SELECT distinct album.title FROM album
+JOIN album_to_performer atp ON atp.albumid = album.id
+JOIN genre_to_performer gtp ON gtp.performerid = atp.performerid
+GROUP BY album.title
+HAVING COUNT(gtp.performerid)>1;
+-- 7. tracks that not included in collections
 SELECT track.title FROM track
 WHERE track.id NOT IN (
     SELECT DISTINCT trackid FROM track_to_collection);
--- performers with shortest tracks
-SELECT performer.name FROM performer
-WHERE performer.id in (
-	SELECT DISTINCT performerid FROM album_to_performer atp
-	WHERE albumid IN (
-		SELECT albumid FROM track t 
-		WHERE duration = (SELECT MIN(duration) FROM track))
-	);
--- albums with least amount tracks
+-- 8. performers with shortest tracks
+SELECT p.name FROM performer p
+JOIN album_to_performer atp ON p.id = atp.performerid
+RIGHT JOIN track t ON atp.albumid= t.albumid 
+	WHERE duration = (SELECT MIN(duration) FROM track)
+;
+-- 9. albums with least amount tracks
 SELECT DISTINCT album.title FROM album , track 
 WHERE album.id = (
 	SELECT albumid FROM (
