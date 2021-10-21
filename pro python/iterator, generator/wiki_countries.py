5f2cfc958df9f66e1f5e3086faf8f837288cbe5a
SamayaA <asadullaevasamaya@gmail.com>
2021-10-21 21:14:59 +0300

    Delete the main code

--- pro python/iterator, generator/wiki_countries.py
+++ pro python/iterator, generator/wiki_countries.py
@@ -21,9 +21,3 @@ class Wiki_countries :
         link = self.country_link.pop()
         return link
 
-
-country_link_f = open("country_wiki.txt", "w+", encoding="utf-8")
-for i in Wiki_countries() :
-   country_link_f.write(i)
-   country_link_f.write('\n')
-country_link_f.close()
