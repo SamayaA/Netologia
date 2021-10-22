c52fd26dfa4c3abeae211f67d063628dc290148d
SamayaA <asadullaevasamaya@gmail.com>
2021-10-22 20:42:22 +0300

    Task 3 completed. The import function was decorated

--- pro python/decorator/main.py
+++ pro python/decorator/main.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 import os
+import sys
+sys.path.append('c:\\Users\\Samaya\\Desktop\\Учеба\\Netology\\OOP\\Netologia\\pro python\\homework_1\\application')
+from salary import calculate_salary
+
 def timeit(log_path):
     def log(func):
         def wraper(*args, **kwargs):
-            with open(log_path,'w+') as f:
-                f.write(f'{datetime.now()}\nfunction name = {func.__name__}\
+            with open(log_path,'a') as f:
+                f.write(f'{datetime.now()}\
+                    \nfunction name = {func.__name__}\
                     \nargs = {args}\tkwargs = {kwargs}\n')
                 result = func(*args, **kwargs)
-                f.write(f'return value = {result}')
+                f.write(f'return value = {result}\n\n')
             return result
         return wraper
     return log
@@ -20,6 +25,9 @@ def foo(*args, **kwargs):
 if __name__ == "__main__":
     l = [i for i in range(5)]
     foo(*l)
+    #task 3
+    a = timeit(os.getcwd() + '\\file.log')(calculate_salary)()
+
 
 
 
