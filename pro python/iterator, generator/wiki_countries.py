c6f928bbc39a7d8fc0d23c683bc6c28d280eec78
SamayaA <asadullaevasamaya@gmail.com>
2021-10-26 17:32:13 +0300

    Replaced form the link in __next__ added __init__ arg - name of json file

--- pro python/iterator, generator/wiki_countries.py
+++ pro python/iterator, generator/wiki_countries.py
@@ -1,23 +1,25 @@
 import json
 
 class Wiki_countries :
-    def __init__ (self):
+    def __init__ (self, file_path ):
         self.base = "https://en.wikipedia.org/wiki/" 
+        self.file_path = file_path
 
     def replace_spaces(self, country: str):
         return country.replace(" ", "_")
 
     def __iter__(self):
-        f = open("countries.json", "r")
+        f = open(self.file_path, "r")
         countries_info = json.load(f)
-        self.country_link = [i["name"]["common"] + ' - ' + self.base + \
-            self.replace_spaces(i["name"]["common"]) for i in countries_info]
+        self.country_link = [i["name"]["common"] for i in countries_info]
         f.close()
         return self
 
     def __next__(self):
         if not self.country_link:
             raise StopIteration
-        link = self.country_link.pop()
+        country = self.country_link.pop()
+        link = country + ' - ' + self.base + \
+            self.replace_spaces(country)
         return link
 
