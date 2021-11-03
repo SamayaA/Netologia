9021f6a870688cbbb731bb6abda42d1ebf5561bd
SamayaA <asadullaevasamaya@gmail.com>
2021-11-03 22:46:51 +0300

    Tested folder exist case

--- pro python/test/test2.py
+++ pro python/test/test2.py
@@ -4,7 +4,7 @@ from YaUpload import YaUpload
 
 class TestYaUpload(unittest.TestCase):
     def setUp(self) -> None:
-        token = ...
+        token = "AQAAAAAPQM_IAADLW2dT7_OrREV9k4loMsl7wxM"
         self.obj = YaUpload(token)
     
     def tearDown(self) -> None:
@@ -12,7 +12,7 @@ class TestYaUpload(unittest.TestCase):
 
     def test_upload_dir(self):
         path_to_dir = "yaUpload"
-        self.assertEqual(str(self.obj.upload_dir(path_to_dir)),"<Response [201]>")
+        self.assertIn(str(self.obj.upload_dir(path_to_dir)),["<Response [201]>", "<Response [409]>"])
 
     if __name__ == "__main__":
         unittest.main()
