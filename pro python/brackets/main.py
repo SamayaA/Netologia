2e410d8354c2e4c66e1a02c8df527040124b8fab
SamayaA <asadullaevasamaya@gmail.com>
2021-11-04 18:07:55 +0300

    Function that checks the balance of brackets

---
+++ pro python/brackets/main.py
@@ -0,0 +1,38 @@
+from stack import stack
+
+def check_balance(brackets:str) -> bool:
+    '''The argument brackets is the string of brackets.
+    For example, "[[{())}]".
+    Output:
+    If brackets balanced "{{[()]}}" - True
+    otherwise "[[{())}]" - False
+    '''
+    brackets_stack = stack()
+    for i in range(len(brackets)):
+        if brackets_stack.size() == 0:
+            brackets_stack.push(brackets[i])
+            continue
+        if brackets[i] == ")" and brackets_stack.peek() == "(":
+            brackets_stack.pop()
+            continue
+        elif brackets[i] == "]" and brackets_stack.peek() == "[":
+            brackets_stack.pop()
+            continue
+        elif brackets[i] == "}" and brackets_stack.peek() == "{":
+            brackets_stack.pop()
+            continue
+        else:
+            brackets_stack.push(brackets[i])
+    return brackets_stack.is_empty()
+
+
+if __name__ == "__main__":
+    expression = input("Введите сторку, содержащую только скобки: ")
+    balance = check_balance(expression)
+    if (balance):
+        print("Сбалансированно")
+    else:
+        print("Несбалансированно")
+
+        
+
